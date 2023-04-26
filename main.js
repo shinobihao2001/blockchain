d412f3ab1cf7800c458608eb2567c40175d7671a
Gia Hao <shinobihao2001@gmail.com>
2023-04-27 00:03:57 +0700

    init_block

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const SHA256 = require("crypto-js/sha256");
+
 class Block {
   constructor(index, timestamp, data, previousHash = "") {
     this.index = index;
@@ -7,5 +9,34 @@ class Block {
     this.hash = "";
   }
 
-  calculateHash() {}
+  calculateHash() {
+    return SHA256(
+      this.index +
+        this.previousHash +
+        this.timestamp +
+        JSON.stringify(this.data)
+    ).toString();
+  }
 }
+
+class Blockchain {
+  constructor() {
+    this.chain = [this.createGenesisBlock()];
+  }
+
+  createGenesisBlock() {
+    return new Block(0, "26/04/2023", "Genesis block", "0");
+  }
+
+  getLatestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  addBlock(newBlock) {
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.hash = newBlock.calculateHash();
+    this.chain.push(newBlock);
+  }
+}
+
+let ltvCoin = new Blockchain();
